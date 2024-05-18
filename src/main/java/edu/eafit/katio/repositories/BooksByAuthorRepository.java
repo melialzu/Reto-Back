c591a36a8e5633b3407e19faf1dab682403f7340
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2024-05-18 08:53:35 -0500

    Create controllers and services requered

--- src/main/java/edu/eafit/katio/repositories/BooksByAuthorRepository.java
+++ src/main/java/edu/eafit/katio/repositories/BooksByAuthorRepository.java
@@ -1,43 +1,53 @@
 package edu.eafit.katio.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import edu.eafit.katio.dtos.BooksByAuthor;
 
-public interface BooksByAuthorRepository extends JpaRepository<BooksByAuthor, Long> {
-    
+public interface BooksByAuthorRepository extends CrudRepository<BooksByAuthor, Long> {
+
+    // Find by Id
     @Query(nativeQuery = true,
-        value =
-            "SELECT " +
-                "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, "+
-                "au.LastName as Author_Name " +
+    value = "SELECT " +
+            "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, " +
+            "au.LastName as Author_Name " +
             "FROM Authors au " +
             "JOIN Books bk ON bk.author_id = au.id " +
-            "WHERE " +
-                "au.Lastname LIKE %:lastname%")
-    Iterable<BooksByAuthor> findByAuthorLastName(@Param("lastname") String LastName);
+            "WHERE au.id = :idAuthor")
+    Iterable<BooksByAuthor> findByAuthorId(@Param("idAuthor") Integer idAuthor);
+
+    // Find by Name
+    @Query(nativeQuery = true,
+    value = "SELECT " +
+            "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, " +
+            "au.LastName as Author_Name " +
+            "FROM Authors au " +
+            "JOIN Books bk ON bk.author_id = au.id " +
+            "WHERE au.Name LIKE %:name%")
+    Iterable<BooksByAuthor> findByAuthorName(@Param("name") String name);
 
     @Query(nativeQuery = true,
-        value =
-            "SELECT " +
-                "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, "+
+        value = "SELECT " +
+                "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, " +
                 "au.LastName as Author_Name " +
-            "FROM Authors au " +
-            "JOIN Books bk ON bk.author_id = au.id " +
-            "WHERE " +
-                "au.Name LIKE %:name%")
-    Iterable<BooksByAuthor> findByAuthorName(@Param("name") String Name);
+                "FROM Authors au " +
+                "JOIN Books bk ON bk.author_id = au.id " +
+                "WHERE au.LastName LIKE %:lastname%")
+    Iterable<BooksByAuthor> findByAuthorLastName(@Param("lastname") String lastName);
 
+
+    // Find by FullName
     @Query(nativeQuery = true,
-        value =
-            "SELECT " +
-                "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, "+
+        value = "SELECT " +
+                "bk.id as Id_Book, bk.Name as Book_Name, bk.ISBN13, " +
                 "au.LastName as Author_Name " +
-            "FROM Authors au " +
-            "JOIN Books bk ON bk.author_id = au.id " +
-            "WHERE " +
-                "au.Lastname LIKE %:lastname% OR au.Name LIKE %:name%")
-    Iterable<BooksByAuthor> findByAuthorFullName(@Param("lastname") String LastName, @Param("name") String Name);
+                "FROM Authors au " +
+                "JOIN Books bk ON bk.author_id = au.id " +
+                "WHERE au.LastName LIKE %:lastname% OR au.Name LIKE %:name%")
+    Iterable<BooksByAuthor> findByAuthorFullName(@Param("lastname") String lastNameAuthor, @Param("name") String nameAuthor);
+
+
 }
+
