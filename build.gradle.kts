87cfff8e72b814d39770231b4afb4c42aeba87da
Melissa <melissa.alvarez.zuluaga@gmail.com>
2024-06-10 23:45:39 -0500

    Se realizan los ajustes para poder obtener el token JWT, se agregan las porpiedades en el aplication.properties, adicionalmente se modifincan las dependencias en el build.gradle. se crean nuevos dtos, nuevas interfaces, y se implementa en la clase Usuarios Usersdetails

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,15 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("io.jsonwebtoken:jjwt:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+}
+tasks.withType<JavaCompile>() {
+    options.compilerArgs.add("-parameters")
 }
 
 tasks.withType<Test> {
