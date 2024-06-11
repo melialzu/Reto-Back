f2572f633200324de3b7964341338ef26feb9dce
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2024-06-11 10:47:42 -0500

    Create dto, edit create author, create edit author, create finbybirthdate

--- src/main/java/edu/eafit/katio/interfaces/BaseAuthorService.java
+++ src/main/java/edu/eafit/katio/interfaces/BaseAuthorService.java
@@ -1,14 +1,23 @@
 package edu.eafit.katio.interfaces;
 
+import java.util.List;
 import java.util.Optional;
 
-
+import edu.eafit.katio.dtos.AudioBooksByAuthor;
 import edu.eafit.katio.models.Authors;
 
 public interface BaseAuthorService {
 
-     Authors addAuthors(Authors authors);
      Optional<Authors> getAuthorById(Long id);
      Iterable<Authors> getAuthorByName(String name);
      Iterable<Authors> getAuthorByCountry(String country);
+
+
+     Authors addAuthors(Authors authors);
+
+     Authors updateAuthor(String name, Authors updateAuthors);
+
+     List<AudioBooksByAuthor> getAllAudioBooksByAuthorId(Integer idAuthor);
+     List<AudioBooksByAuthor> getAllAudioBooksByAuthorName(String nameAuthor);
+     List<AudioBooksByAuthor> getAllAudioBooksByAuthor(String nameAuthor, String lastNameAuthor);
 }
--- src/main/java/edu/eafit/katio/repositories/AuthorRepository.java
+++ src/main/java/edu/eafit/katio/repositories/AuthorRepository.java
@@ -1,7 +1,13 @@
 package edu.eafit.katio.repositories;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import edu.eafit.katio.models.Authors;
 
 public interface AuthorRepository extends CrudRepository<Authors, Long> {
@@ -11,9 +17,18 @@ public interface AuthorRepository extends CrudRepository<Authors, Long> {
 
     @Query("SELECT a FROM Authors a WHERE a.Country = :country")
     Iterable<Authors> findByCountry(String country);
-
- 
-
     
-   
+    @Query(
+        nativeQuery = true, 
+        value = "SELECT * FROM Authors a WHERE a.Name = :name")
+    Optional<Authors> findByNameOpt(@Param("name") String name);
+
+        @Query(
+        nativeQuery = true,
+        value = "SELECT * FROM Authors WHERE Birthdate BETWEEN :startDate AND :endDate"
+    )
+    List<Authors> findByPublishedDateBetween(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
+
+
+    Authors saveAndFlush(Authors authors);
 }
